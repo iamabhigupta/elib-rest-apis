444d301cd9062a4e8c9d528253d2aa4b9760621c
iamabhigupta <abhishekgupta.july@gmail.com>
2024-06-09 01:02:00 +0530

    setup express server

--- server.ts
+++ server.ts
@@ -1,1 +1,11 @@
-console.log('Abhishek');
+import app from "./src/app";
+
+const startServer = () => {
+  const port = process.env.PORT || 5000;
+
+  app.listen(port, () => {
+    console.log(`Listing on port: ${port}`);
+  });
+};
+
+startServer();
