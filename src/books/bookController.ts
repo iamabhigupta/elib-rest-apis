5abc15b2b14cee2105f5e7d755695f68708e2583
iamabhigupta <abhishekgupta.july@gmail.com>
2024-06-22 00:14:26 +0530

    create book

--- src/books/bookController.ts
+++ src/books/bookController.ts
@@ -1,20 +1,26 @@
-import { NextFunction, Request, Response } from "express";
-import cloudinary from "../config/cloudinary";
+import fs from "node:fs";
 import path from "node:path";
+
+import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 
+import cloudinary from "../config/cloudinary";
+import bookModel from "./bookModel";
+
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
+  const { title, genre } = req.body;
+
+  const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+
+  const coverImageMimeType = files.coverImage[0].mimetype.split("/")[-1];
+  const fileName = files.coverImage[0].filename;
+  const filePath = path.resolve(
+    __dirname,
+    "../../public/data/uploads",
+    fileName
+  );
+
   try {
-    const files = req.files as { [fieldname: string]: Express.Multer.File[] };
-
-    const coverImageMimeType = files.coverImage[0].mimetype.split("/")[-1];
-    const fileName = files.coverImage[0].filename;
-    const filePath = path.resolve(
-      __dirname,
-      "../../public/data/uploads",
-      fileName
-    );
-
     const uploadResult = await cloudinary.uploader.upload(filePath, {
       filename_override: fileName,
       folder: "book-covers",
@@ -41,7 +47,19 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
     console.log({ uploadResult });
     console.log({ bookFileUploadResult });
 
-    res.json({ message: "Ok" });
+    const newBook = await bookModel.create({
+      title,
+      genre,
+      author: "666bd24534bd84d517b1574b",
+      coverlmage: uploadResult.secure_url,
+      file: bookFileUploadResult.secure_url,
+    });
+
+    // Delete temp files
+    await fs.promises.unlink(filePath);
+    await fs.promises.unlink(bookFilePath);
+
+    res.status(201).json({ id: newBook._id });
   } catch (error) {
     console.log(error);
     return next(createHttpError(500, "Error while uploading the book files"));
